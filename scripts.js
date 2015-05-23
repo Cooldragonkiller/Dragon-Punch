af7e377bb08c29b62ee2eeb5b027fab136e2ea87
Cooldragonkiller <taptap@live.no>
2015-05-23 18:12:19 +0200

    Changed dragon picture for dragon1 and dragon2

--- scripts.js
+++ scripts.js
@@ -9,9 +9,9 @@ function whoToPunch(punchTarget, numberOfTimes){
 	}
 }
 
-whoToPunch("<img src=\"http://th04.deviantart.net/fs5/PRE/i/2004/286/e/8/cool_dragon_by_Dokuro.jpg\"/>", 4);
+whoToPunch("<img src=\"http://i.imgur.com/Fa5HjTJ.png\"/>", 4);
 
-whoToPunch("<img src=\"http://fc02.deviantart.net/fs71/f/2012/031/a/a/cool_story_bro__needs_more_dragon_types_by_e4animaton-d4o6wml.png\"/>", 1);
+whoToPunch("<img src=\"http://i.imgur.com/hCNbvTP.png\"/>", 1);
 
 var num = 19;
 
