b0bce259254580279dc2426263211786e75fa2b4
Cooldragonkiller <taptap@live.no>
2015-05-23 17:46:38 +0200

    Changed number of dragons getting punced, added dragons

---
+++ scripts.js
@@ -0,0 +1,19 @@
+document.write("punch dragons<br/>");
+document.write("<br/>wait no");
+
+function whoToPunch(punchTarget, numberOfTimes){
+    	
+	for (var index = 0; index < numberOfTimes; index = index + 1){
+		document.write("<br/><br/>punch "+punchTarget);
+		
+	}
+}
+
+whoToPunch("<img src=\"http://th04.deviantart.net/fs5/PRE/i/2004/286/e/8/cool_dragon_by_Dokuro.jpg\"/>", 4);
+
+whoToPunch("<img src=\"http://fc02.deviantart.net/fs71/f/2012/031/a/a/cool_story_bro__needs_more_dragon_types_by_e4animaton-d4o6wml.png\"/>", 1);
+
+var num = 19;
+
+
+
