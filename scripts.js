7e3325dcc679b91ed8b711323972ad148409fce1
Cooldragonkiller <taptap@live.no>
2015-05-26 21:14:52 +0200

    RIGHTLEFTUPDOWN test 3

--- scripts.js
+++ scripts.js
@@ -22,11 +22,11 @@ function update() {
 	}
 	
 	if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-		player.y = player.y + 1;
+		player.x = player.y - 1;
 	}
 	
 	if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-		player.y = player.y - 1;
+		player.y = player.y + 1;
 	}
 		
 		
