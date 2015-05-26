e95d1f5de3da3a4fab6feb2f03fcfe9ce81dcc9b
Cooldragonkiller <taptap@live.no>
2015-05-26 22:04:17 +0200

    Added background, boundaries and movespeed

--- scripts.js
+++ scripts.js
@@ -2,32 +2,55 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, "dragon-punch", {
 	preload: preload, create: create, update: update
 });
 
+var movespeed = 5;
+
 var player;
 
 function preload() {
 	game.load.image("player","Images/small-fry.png");
+	
+	game.load.image("background", "Images/background.png");
+	
 }
 
 function create() {
-	player = game.add.sprite(400,300,"player");
+	game.add.sprite(0, 0, "background" );
+	
+	player = game.add.sprite(400, 450, "player");
+		
 }
 
 function update() {
 	if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-		player.x = player.x + 1;
+		player.x = player.x + movespeed;
 	}
 	
 	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-		player.x = player.x - 1;
+		player.x = player.x - movespeed;
 	}
 	
 	if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-		player.y = player.y + 1;
+		player.y = player.y + movespeed;
 	}
 	
 	if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-		player.y = player.y - 1;
+		player.y = player.y - movespeed;
 	}
 		
-		
+	if (player.y < 450){
+		player.y = 450;
+	}	
+	
+	if (player.x < 0){
+		player.x = 0;
+	}
+	
+	if (player.x > 780){
+		player.x = 780;
+	}
+	
+	if (player.y > 580){
+		player.y = 580;
+	}
+	
 }
